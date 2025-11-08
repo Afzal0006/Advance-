f3d016b61e02b8b592f53eae154fb1dcae553cf0
Afzal0006 <121087528+Afzal0006@users.noreply.github.com>
2025-11-08 10:39:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ BOT_TOKEN = "8399143304:AAFf8jNzZgXmo6NeLQUZhQhEfI1tSi_k1mc"
 MONGO_URI = "mongodb+srv://Escrow_LuckyWorld:Escrow_LuckyWorld@cluster0.c3jkrlx.mongodb.net/?appName=Cluster0"
 LOG_CHANNEL_ID = -1002330347621
 
-OWNER_IDS = [6847499628]  # Add as many owners as needed
+OWNER_IDS = [6998916494]  # Add as many owners as needed
 
 # ==== MONGO CONNECT ====
 client = MongoClient(MONGO_URI)
